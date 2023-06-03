3f9a342f3e49f3d4723b9def84a754823fd198c1
andres <valentinomonzonandres@gmail.com>
2023-06-03 19:12:18 -0300

    add cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import { PORT } from './config.js'
-import indexPage from './routes/index.routes.js'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from "express"
+import { PORT } from "./config.js"
+import indexPage from "./routes/index.routes.js"
+import morgan from "morgan"
+import cors from "cors"
 
 const app = express()
 
 app.use(cors())
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 app.use(express.json())
 
 app.use(indexPage)
